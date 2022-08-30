ff32c7ddc3594d24d43d17b6fb336362ee5cc645
Shannon Warren <shannonw7376@gmail.com>
2022-08-30 15:57:16 -0500

    added storage

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,48 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateNote = /* GraphQL */ `
+  subscription OnCreateNote($filter: ModelSubscriptionNoteFilterInput) {
+    onCreateNote(filter: $filter) {
+      id
+      name
+      description
+      image
+      createdAt
+      updatedAt
+      _version
+      _deleted
+      _lastChangedAt
+    }
+  }
+`;
+export const onUpdateNote = /* GraphQL */ `
+  subscription OnUpdateNote($filter: ModelSubscriptionNoteFilterInput) {
+    onUpdateNote(filter: $filter) {
+      id
+      name
+      description
+      image
+      createdAt
+      updatedAt
+      _version
+      _deleted
+      _lastChangedAt
+    }
+  }
+`;
+export const onDeleteNote = /* GraphQL */ `
+  subscription OnDeleteNote($filter: ModelSubscriptionNoteFilterInput) {
+    onDeleteNote(filter: $filter) {
+      id
+      name
+      description
+      image
+      createdAt
+      updatedAt
+      _version
+      _deleted
+      _lastChangedAt
+    }
+  }
+`;
